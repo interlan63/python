7bfc00078a589ca3021b959d67d6f9e1ff609ffd
interlan63 <104315341+interlan63@users.noreply.github.com>
2023-09-17 21:25:28 +0300

    Update translite.py

--- translite.py
+++ translite.py
@@ -1,10 +1,17 @@
 from transliterate import translit
 from num2words import num2words
 
-text = "Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime and I guess that this is mine. \nPeople have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies. Neither will happen.\n \nMore than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8..."
-ru_text = translit(text, 'ru')
-print(ru_text)
-print("")
-print(num2words(42))
-print(num2words(42, to='ordinal'))
-print(num2words(42, lang='fr'))
+text1 = "Ladies and gentlemen, I'm"
+text2 = "years old and I finally got"
+text3 = "minutes of fame once in a lifetime and I guess that this is mine. \nPeople have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies.\n \n Neither will happen. More than"
+text4 = "years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last"
+text5 = "When I was" 
+text6 = "..."
+ru_text1 = translit(text1, 'ru')
+ru_text2 = translit(text2, 'ru')
+ru_text3 = translit(text3, 'ru')
+ru_text4 = translit(text4, 'ru')
+ru_text5 = translit(text5, 'ru')
+ru_text6 = translit(text6, 'ru')
+output = f"{ru_text1} {num2words(78)} {ru_text2} {num2words(15)} {ru_text3} {num2words(3)} {ru_text4} {num2words(40)} {ru_text5} {num2words(8)} {ru_text6}"
+print(output)
